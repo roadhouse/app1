7000e7ba830a465e8f63c21981a1f137ed55785f
Jean Uchoa <jean.uchoa@gmail.com>
2018-06-22 16:38:35 -0300

    extract domains

--- app/controllers/api/v1/ubs_controller.rb
+++ app/controllers/api/v1/ubs_controller.rb
@@ -1,10 +1,42 @@
+class UbsFinderService
+  def initialize(params, radius)
+    @params = UbsParams.new(params)
+
+    @radius = radius
+  end
+
+  def self.ubs(params, radius = 100)
+    new(params, radius).ubs
+  end
+
+  def ubs
+    Ubs
+      .within(@params.latitude, @params.longitude, @radius)
+      .page(@params.page)
+      .per(@params.per_page)
+  end
+end
+
+class UbsParams
+  attr_reader :latitude, :longitude, :page, :per_page
+
+  def initialize(params)
+    @params = params
+    @latitude, @longitude = @params[:query].split(",").tap { |p| raise ArgumentError, p unless p.size == 2 }
+    @page = @params[:page]
+    @page = @params[:per_page]
+  end
+end
+
 module Api
   module V1
     class UbsController < ApplicationController
       def find_ubs
-        latitude, longitude = params[:query].split(",")
+        render json: UbsFinderService.ubs(find_params).to_json
+      end
 
-        render json: Ubs.within(latitude, longitude, 100).to_json
+      def find_params
+        params.permit(:query,:page, :per_page)
       end
     end
   end
