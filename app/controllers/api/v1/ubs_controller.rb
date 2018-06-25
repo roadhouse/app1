e58e5f094edf2284bdc974e0d7a738d081937b09
Jean Uchoa <jean.uchoa@gmail.com>
2018-06-25 14:48:31 -0300

    add basic exception handling

--- app/controllers/api/v1/ubs_controller.rb
+++ app/controllers/api/v1/ubs_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class UbsController < ApplicationController
+      rescue_from StandardError, with: :render_error
       def find_ubs
         render json: UbsFinderService.ubs(find_params).to_json
       end
@@ -9,6 +10,11 @@ module Api
 
       def find_params
         params.permit(:query, :page, :per_page)
+      end
+
+      def render_error
+        Rails.logger.warn( "erro ")
+        render status:500, json: {erro: "erro no servidor"}.to_json
       end
     end
   end
